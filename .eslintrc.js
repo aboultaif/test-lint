835bdc7543067b813b703ae0b447cb376743e35a
aboultaif <164658341+aboultaif@users.noreply.github.com>
2025-08-06 15:46:26 -0500

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+// .eslintrc.js
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    // Add your custom rules here
+  }
+};
